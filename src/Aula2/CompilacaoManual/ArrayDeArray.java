38ca5f98d8696f3ac84d6f19cddecbf410a6678f
Miguel Silva <miguel.lvl14@gmail.com>
2017-02-24 11:10:04 +0000

    Adicionado Inversão de Arrays

--- src/Aula2/CompilacaoManual/ArrayDeArray.java
+++ src/Aula2/CompilacaoManual/ArrayDeArray.java
@@ -2,12 +2,27 @@ package Aula2.CompilacaoManual;
 
 class ArrayDeArray {
 	public static void main(String[] args) {
-		//System.out.println("Hello World!");
+		System.out.println("Array Normal");
 		int[][] matrix = {{1, 2}, {10, 20, 30}, {11, 21, 31, 41, 51}};
 		for (int i = 0; i <= matrix.length - 1; i++) {
 			for (int j = 0; j <= matrix[i].length - 1; j++) {
 				System.out.println(matrix[i][j]);
 			}
 		}
+		
+		System.out.println("\nJ Invertido");
+		for (int i = matrix.length - 1; i >= 0; i--) {
+			for (int j = 0; j <= matrix[i].length - 1; j++) {
+				System.out.println(matrix[i][j]);
+			}
+		}
+		
+		System.out.println("\nArray Invertido");
+		for (int i = matrix.length - 1; i >= 0; i--) {
+			for (int j = matrix[i].length - 1; j >= 0; j--) {
+				System.out.println(matrix[i][j]);
+			}
+		}
+		
 	}
 }
