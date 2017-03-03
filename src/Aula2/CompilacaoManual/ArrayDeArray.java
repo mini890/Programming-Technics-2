8d6f71f061404f2d8253e9a2506838c7c7aec654
Miguel Silva <miguel.lvl14@gmail.com>
2017-03-03 08:46:25 +0000

    Fix simples. Não ligar

--- src/Aula2/CompilacaoManual/ArrayDeArray.java
+++ src/Aula2/CompilacaoManual/ArrayDeArray.java
@@ -2,8 +2,9 @@ package Aula2.CompilacaoManual;
 
 class ArrayDeArray {
 	public static void main(String[] args) {
-		System.out.println("Array Normal");
 		int[][] matrix = {{1, 2}, {10, 20, 30}, {11, 21, 31, 41, 51}};
+		
+		System.out.println("Array Normal");
 		for (int i = 0; i <= matrix.length - 1; i++) {
 			for (int j = 0; j <= matrix[i].length - 1; j++) {
 				System.out.println(matrix[i][j]);
