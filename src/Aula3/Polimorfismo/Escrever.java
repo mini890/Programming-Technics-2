c3b8daa2be77683370e87a629f2d0a460a032d17
Miguel Silva <miguel.lvl14@gmail.com>
2017-03-03 10:58:17 +0000

    Adicionado Polimorfismo

---
+++ src/Aula3/Polimorfismo/Escrever.java
@@ -0,0 +1,32 @@
+package Aula3.Polimorfismo;
+
+public class Escrever {
+	/**
+	 * Imprime o texto passado como argumento
+	 * @param i
+	 */
+	public static void escreve(int i) {
+		System.out.println("Escreve int:" + i);
+	}
+	/**
+	 * Imprime o texto passado como argumento
+	 * @param s
+	 */
+	public static void escreve(String s) {
+		System.out.println("Escreve String:" + s);
+	}
+	/**
+	 * Imprime o texto passado como argumento
+	 * @param d
+	 */
+	public static void escreve(double d) {
+		System.out.println("Escreve double:" + d);
+	}
+	/**
+	 * Imprime o texto passado como argumento
+	 * @param b
+	 */
+	public static void escreve(boolean b) {
+		System.out.println("Escreve boolean:" + b);
+	}
+}
---
+++ src/Aula3/Polimorfismo/TesteEscrever.java
@@ -0,0 +1,12 @@
+package Aula3.Polimorfismo;
+
+public class TesteEscrever {
+
+	public static void main(String[] args) {
+		Escrever.escreve(true);
+		Escrever.escreve(50D);
+		Escrever.escreve(50);
+		Escrever.escreve("Hello World");
+	}
+	
+}
